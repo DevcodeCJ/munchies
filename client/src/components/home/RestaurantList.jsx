a0925c74c0900d81c77ace7ed4007065b4e42b2b
CJ <chiji.davids@yahoo.com>
2021-06-03 08:22:43 -0600

    initialized React and added styles

---
+++ client/src/components/home/RestaurantList.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+function RestaurantList() {
+  return (
+    <div className="list-group">
+      <table className="table">
+        <thead>
+          <tr className="bg-primary table-head-row">
+            <th scope="col">Restaurant</th>
+            <th scope="col">Location</th>
+            <th scope="col">Price Range</th>
+            <th scope="col">Ratings</th>
+            <th scope="col">Edit</th>
+            <th scope="col">Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>McDonalds</td>
+            <td>New York</td>
+            <td>$$</td>
+            <td>Rating</td>
+            <td>
+              <button className="btn btn-warning">Update</button>
+            </td>
+            <td>
+              <button className="btn btn-danger">Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default RestaurantList;
